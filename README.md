5789fa99df4855b52b14509a6ad218a55eb0f4f6
Kevin Gimbel <hallo@kevingimbel.com>
2017-09-07 09:01:30 +0200

    [FLAG] add -verbose flag

--- README.md
+++ README.md
@@ -1,25 +1,35 @@
 # goserve
+> Static file server written in go.
 
-Static file server written in go.
+## Installation
 
+### From archive
 
-### Install
+Go to the [release page](https://github.com/kevingimbel/goserve/releases) and download a binary for your operating system.
+`goserve` is build for Windows, MacOS and Linux with [goreleaser](https://github.com/goreleaser/goreleaser).
+
+### From source
+
 1. [Install Go](https://golang.org/doc/install#install).
 2. Clone the repo `git clone https://github.com/kevingimbel/goserve.git`
 3. Run `go build goserve.go` from within the new directory.
 
-If you would like to run the program from everywhere, link it into you `$PATH` variable, e.g.:
+Place the binary somewhere in your `$PATH`, e.g.
 ```sh
   $ sudo ln -s $(pwd)/goserve /usr/local/bin
 ```
 
 ### Usage
+
 `goserve` can be used from the command line as follows:
 
 ```sh
-  $ goserve [-port ""]
+  $ goserve [-port ""] [-verbose]
 ```
-This will serve the current directory to `localhost:8000` or the specified port.
+This will serve the current directory to `localhost:8000` or the specified port. `-verbose` logs each request to Stdout.
+
+See also `goserve -help`.
 
 ### Test it
-To test the server run `goserve` from the project directory and open [localhost:8000/example](localhost:8000/example)
+
+To test the server run `goserve` from the project directory and open [localhost:8000/example](localhost:8000/example).
--- goserve.go
+++ goserve.go
@@ -1,88 +1,90 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "flag"
-  "strings"
+	"flag"
+	"fmt"
+	"net/http"
+	"strings"
 )
 
 // Parameter structure
 type Parameters struct {
-  port string
-  route string
+	port  string
+	route string
 }
 
-var port  = flag.String("port", ":8000", "Defines the port to serve to.")
+var port = flag.String("port", ":8000", "Defines the port to serve to.")
+var verbose = flag.Bool("verbose", false, "Turn on verbose logging")
 
 // logRequest logs part of the HTTP Request to the
 // command line
 func logRequest(request *http.Request) {
-  fmt.Println(
-    "\n Method", request.Method, "\n",
-    "Host", request.Host, "\n",
-    "Referer", request.Header["Referer"], "\n",
-    "User-Agent:", request.Header["User-Agent"], "\n")
+	if *verbose {
+		fmt.Println("\n",
+			"Method", request.Method, "\n",
+			"Host", request.Host, "\n",
+			"Referer", request.Header["Referer"], "\n",
+			"User-Agent:", request.Header["User-Agent"])
+	}
 }
 
 // handles serving of files through http
 func handler(w http.ResponseWriter, r *http.Request) {
-  route := r.URL.Path[1:]
-  http.ServeFile(w, r, route)
-
-  logRequest(r)
+	route := r.URL.Path[1:]
+	http.ServeFile(w, r, route)
+	logRequest(r)
 }
 
 // initialize the server with the Parameters object
 func initializeServer(params *Parameters) {
-  // Get the port and route from Parameters Objects
-  port := params.port
-  route := params.route
+	// Get the port and route from Parameters Objects
+	port := params.port
+	route := params.route
 
-  fmt.Println("Serving on port", port)
+	fmt.Println("Serving on port", port)
 
-  http.HandleFunc(route, handler)
-  http.ListenAndServe(port, nil)
+	http.HandleFunc(route, handler)
+	http.ListenAndServe(port, nil)
 }
 
 // Normalize the port by adding a colon (:) in front of
 // the port number so it can be used with http.ListenAndServe
-func normalizePort(old_port string)string {
-  // get the old port
-  new_port := old_port
+func normalizePort(old_port string) string {
+	// get the old port
+	new_port := old_port
 
-  // Check if there is a colon (:) inside
-  index := strings.Index(new_port, ":")
-  // If not, prepend one
-  if index == -1 {
-    new_port = ":" + new_port;
-  }
-  // return the normalized port
-  return new_port;
+	// Check if there is a colon (:) inside
+	index := strings.Index(new_port, ":")
+	// If not, prepend one
+	if index == -1 {
+		new_port = ":" + new_port
+	}
+	// return the normalized port
+	return new_port
 }
 
 // Generate the Parameters Object* from flags
 func generateParameterObject() *Parameters {
-  // Parse command line flags
-  flag.Parse()
+	// Parse command line flags
+	flag.Parse()
 
-  // Normalize the port.
-  port := normalizePort(*port)
+	// Normalize the port.
+	port := normalizePort(*port)
 
-  // Create a new Parameters instance
-  params := new(Parameters)
+	// Create a new Parameters instance
+	params := new(Parameters)
 
-  // Assign port and entry point
-  params.port = port
-  // route is static for now.
-  params.route = "/"
+	// Assign port and entry point
+	params.port = port
+	// route is static for now.
+	params.route = "/"
 
-  // Return the Parameters instance
-  return params
+	// Return the Parameters instance
+	return params
 }
 
 // initialize the parameters and start the server.
 func main() {
-  params := generateParameterObject()
-  initializeServer(params)
+	params := generateParameterObject()
+	initializeServer(params)
 }
